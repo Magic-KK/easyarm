88c4364c9a7a501057129303791f01bb52d1f60a
ke.zhang <ke.zhang-ext@ecarxgroup.com>
2022-08-03 17:14:32 +0800

    updata: 还原json容错

--- EasyFrame/src/main/java/com/example/easyframe/toolmodel/EasyGsonUtil.java
+++ EasyFrame/src/main/java/com/example/easyframe/toolmodel/EasyGsonUtil.java
@@ -18,7 +18,7 @@ public class EasyGsonUtil {
 
     static {
         if (gson == null) {
-            gson = GsonFactory.getSingletonGson();
+            gson = new Gson();
         }
     }
 
---
+++ app/src/main/java/com/example/easyframe/login/testbean.java
@@ -0,0 +1,2 @@
+package com.example.easyframe.login;public class testbean {
+}
